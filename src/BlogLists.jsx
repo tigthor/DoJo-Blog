91cf3204134da55a99d3994cf855e4966a4a20ae
tigthor <tigthor1337@gmail.com>
2021-02-27 06:17:27 +0200

    Add BlogLists and their props

---
+++ src/BlogLists.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+const BlogList = ({blogs, title, handleDelete}) => {  
+    return ( 
+        <div className="blog-list">
+            <h2>{title}</h2>
+            {blogs.map((blog) => (
+          <div className="blog-preview" key={blog.id}>
+              <h2>{blog.title}</h2>
+              <p>Writen by {blog.author}</p>
+              <button onClick={() => handleDelete(blog.id)}>Delete blog</button>
+          </div>
+      ))}
+        </div>
+     );
+}
+ 
+export default BlogList;
+
